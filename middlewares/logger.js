1228bdb0b1ee6c28c7fbeec4a841e70bae2e7783
abhimanyuspark <127104226+abhimanyuspark@users.noreply.github.com>
2025-02-15 20:42:41 +0530

    fix logger date format to use uppercase 'MM' for month

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -5,7 +5,7 @@ const { format } = require("date-fns");
 const { v4: uuid } = require("uuid");
 
 const logEvents = async (message, logFileName) => {
-  const date = format(new Date(), "dd/mm/yyyy\thh:mm:ss");
+  const date = format(new Date(), "dd/MM/yyyy\thh:mm:ss");
   const data = `${date}\t${uuid()}\t${message}\n`;
 
   try {
